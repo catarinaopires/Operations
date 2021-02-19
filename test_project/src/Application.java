80602ebf62eb5d658ab623da4b3f220cdfc9a8cf
Catarina Pires <catarina.o.pires@hotmail.com>
2021-02-19 15:23:07 +0000

    Added more operations

--- test_project/src/Application.java
+++ test_project/src/Application.java
@@ -4,9 +4,19 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        if (operation.equals("sum"))
+            System.out.println(a + b);
+        else if (operation.equals("dif"))
+            System.out.println(a - b);
+        else if (operation.equals("mul"))
+            System.out.println(a * b);
+        else if (operation.equals("div"))
+            System.out.println(a / b);
+        else
+            System.out.println("Invalid operation.");
     }
 }
