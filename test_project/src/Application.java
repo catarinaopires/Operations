0d9243981ef83c0d340dbac5b02961b826b57958
Catarina Pires <catarina.o.pires@hotmail.com>
2021-02-19 15:35:14 +0000

    Improved interface

--- test_project/src/Application.java
+++ test_project/src/Application.java
@@ -4,21 +4,21 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter operation: ");
         String operation = scanner.nextLine();
+        System.out.println("Enter 2 integers: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
 
-        System.out.println("Result: " + (a + b));
-
         if (operation.equals("sum"))
-            System.out.println(a + b);
+            System.out.println("Result: " + (a + b));
         else if (operation.equals("dif"))
-            System.out.println(a - b);
+            System.out.println("Result: " + (a - b));
         else if (operation.equals("mul"))
-            System.out.println(a * b);
+            System.out.println("Result: " + (a * b));
         else if (operation.equals("div"))
-            System.out.println(a / b);
+            System.out.println("Result: " + (a / b));
         else
             System.out.println("Invalid operation.");
     }
